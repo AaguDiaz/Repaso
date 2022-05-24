af2f825c3d853680b8cc55a3c51e0e13cd1b7d2a
Agustin <Agustin@w10ntbx-sloti.acacoop.net>
2022-05-24 10:05:46 -0300

    oa

---
+++ clase de repaso1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("Ingresar primer numero: ");
+int numero1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingresar segundo numero: ");
+int numero2 = int.Parse(Console.ReadLine());
+int div;
+try
+{
+    div = numero1 / numero2;
+    Console.WriteLine("El resultado es: "+ div);
+
+}catch(DivideByZeroException ex)when(numero2==0)
+{
+    Console.WriteLine("No se puede dividir por 0");
+}
+finally 
+{
+    
+}
